e1a4dbbd18713d65fadababc950820f9177cf397
Matthew Lutz <gothx2009@gmail.com>
2014-12-28 00:26:24 -0600

    Added connection for mysqli

--- game/db.php
+++ game/db.php
@@ -1,4 +1,10 @@
-<? 
+<?php
+  // New mysqli database connection
+  $db = new mysqli("localhost", "root", "", "webrpg");
+  if($db->connect_error) {
+    die($db->connect_error);
+  }
+  
 $i=10;
 while ((!($erg=mysql_pconnect("localhost","root","")))&&($i!=0)){$i--;}
 if (!$erg) DIE ("Unable to connect to database.");
